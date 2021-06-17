1182d90523109df296a9fc17b7e8e2d8e0880e33
moregorenine <moregorenine@gmail.com>
2021-06-18 01:06:16 +0900

    init project

---
+++ src/kr/boj/stages/s1_IO_and_arithmetic_operations/java/S1HelloWorld.java
@@ -0,0 +1,8 @@
+package kr.boj.stages.s1_IO_and_arithmetic_operations.java;
+
+public class S1HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
