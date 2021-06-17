101f878e078d423941cb12f47a939b83446b6af8
moregorenine <moregorenine@gmail.com>
2021-06-18 01:24:28 +0900

    feature s1/q1/HelloWorld

--- src/kr/boj/stages/s1_IO_and_arithmetic_operations/q1_Hello_World/java/Main.java
+++ src/kr/boj/stages/s1_IO_and_arithmetic_operations/q1_Hello_World/java/Main.java
@@ -1,6 +1,6 @@
-package kr.boj.stages.s1_IO_and_arithmetic_operations.java;
+package kr.boj.stages.s1_IO_and_arithmetic_operations.q1_Hello_World.java;
 
-public class S1HelloWorld {
+public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
